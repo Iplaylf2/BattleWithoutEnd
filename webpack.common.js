ffdb5a148e2a7d0c9343050a33ccefef62228f94
IplayLF2 <914637118@qq.com>
2019-03-22 23:49:20 +0800

    webpack的externals没配好

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
         libraryExport: "default"
     },
     externals: {
-        React: 'React'
+        react: 'React'
     },
     plugins: [
         new CleanWebpackPlugin(),
