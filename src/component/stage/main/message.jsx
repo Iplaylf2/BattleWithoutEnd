c003ef3662f163864fd2a7436e4f42d6de4e1df3
IplayLF2 <914637118@qq.com>
2019-03-23 23:00:06 +0800

    message用ul、li展示数据，为了避免diff，直接操作dom

--- src/component/stage/main/message.jsx
+++ src/component/stage/main/message.jsx
@@ -1,13 +1,18 @@
-import { useContext } from 'react'
+import { useContext, useRef, useState, useEffect, useMemo } from 'react'
 import GameContext from '../../gameContext.js'
 
 export default function () {
     const value = useContext(GameContext);
-    return (
+    const render = useState({})[1].bind(null, {});
+    const ref = useRef();
+    useEffect(() => {
+        const li = document.createElement('li');
+        li.textContent = value;
+        ref.current.appendChild(li);
+    });
+    const dom = useMemo(() =>
         <div>
-            <ul>
-                <li></li>
-            </ul>
-        </div>
-    );
+            <ul ref={ref}></ul>
+        </div>, [0]);
+    return dom;
 }
