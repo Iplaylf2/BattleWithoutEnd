b8dd0c6a351b8fa823d36ea5bdb2d20860d89c40
IplayLF2 <914637118@qq.com>
2019-03-25 17:40:27 +0800

    monster基础数据

---
+++ src/game/data/raw/monster/monster.js
@@ -0,0 +1,245 @@
+export const Young_Raccoon=new MonsterData("Young Raccoon", "小浣熊", 10, 1, 3, 0, 0, 5, 150, 30, 70);
+
+export const Young_Gray_Raccoon=new MonsterData("Young Gray Raccoon", "小灰浣熊", 20, 2, 4, 2, 3, 10, 150, 50, 80);
+
+export const Young_Brown_Fox=new MonsterData("Young Brown Fox", "小棕狐狸", 20, 2, 5, 2, 3, 10, 150, 50, 90);
+
+export const White_Spiderling=new MonsterData("White Spiderling", "小白蜘蛛", 30, 5, 10, 2, 3, 20, 200, 50, 96);
+
+export const White_Spider=new MonsterData("White Spider", "白蜘蛛", 50, 5, 10, 5, 5, 30, 200, 50, 105);
+
+export const Brown_Fox=new MonsterData("Brown Fox", "棕狐狸", 50, 5, 10, 10, 2, 20, 150, 50, 110);
+
+export const Young_Red_Fox=new MonsterData("Young Red Fox", "小红狐狸", 70, 5, 10, 10, 5, 20, 150, 50, 120);
+
+export const Black_Town_Rat=new MonsterData("Black Town Rat", "黑鼠", 80, 10, 15, 15, 5, 20, 150, 50, 190);
+
+export const Brown_Town_Rat=new MonsterData("Brown Town Rat", "棕鼠", 80, 15, 25, 10, 15, 30, 150, 50, 200);
+
+export const Raccoon=new MonsterData("Raccoon", "浣熊", 100, 20, 25, 10, 5, 30, 150, 50, 135);
+
+export const Old_Mimic=new MonsterData("Old Mimic", "大嘴兽", 120, 10, 40, 10, 5, 80, 250, 50, 145);
+
+export const Red_Spiderling=new MonsterData("Red Spiderling", "小红蜘蛛", 130, 10, 50, 10, 5, 30, 150, 50, 160);
+
+export const Young_Gray_Fox=new MonsterData("Young Gray Fox", "小灰狐狸", 140, 1, 60, 10, 15, 30, 150, 50, 189);
+
+export const Giant_Spiderling=new MonsterData("Giant Spiderling", "巨型蜘蛛", 150, 10, 30, 20, 15, 40, 150, 50, 220);
+
+export const Red_Fox=new MonsterData("Red Fox", "红狐狸", 200, 20, 35, 15, 15, 40, 150, 50, 250);
+
+export const White_Kiwi=new MonsterData("White Kiwi", "白头鸟", 400, 30, 45, 10, 25, 40, 150, 50, 270);
+
+export const Black_Kiwi=new MonsterData("Black Kiwi", "黑翅鸟", 300, 30, 35, 30, 15, 40, 150, 50, 290);
+
+export const Green_Kiwi=new MonsterData("Green Kiwi", "绿翼鸟", 250, 50, 85, 10, 5, 100, 250, 50, 300);
+
+export const Gold_Kiwi=new MonsterData("Gold Kiwi", "黄金鸟", 350, 40, 45, 10, 5, 40, 150, 50, 330);
+
+export const Old_Sand_Mimic=new MonsterData("Old Sand Mimic", "吞噬兽", 300, 10, 100, 10, 25, 40, 150, 50, 340);
+
+export const Young_Goblin=new MonsterData("Young Goblin", "小哥布林", 400, 10, 60, 10, 25, 40, 150, 50, 350);
+
+export const Dingo=new MonsterData("Dingo", "野狗", 200, 60, 150, 10, 15, 40, 150, 50, 370);
+
+export const Small_Ice_Worm=new MonsterData("Small Ice Worm", "小天冰蚕", 300, 50, 55, 50, 5, 40, 150, 50, 400);
+
+export const Stone_Mimic=new MonsterData("Stone Mimic", "拟石者", 350, 50, 55, 10, 50, 40, 150, 50, 420);
+
+export const Young_Poison_Goblin=new MonsterData("Young Poison Goblin", "小毒化哥布林", 500, 50, 55, 20, 15, 40, 150, 50, 440);
+
+export const Brown_tailed_Mongoose=new MonsterData("Brown-tailed Mongoose", "棕尾猫鼬", 350, 70, 95, 10, 25, 100, 250, 50, 450);
+
+export const White_Ant_Lion=new MonsterData("White Ant Lion", "蚁狮", 330, 40, 65, 50, 45, 20, 150, 50, 489);
+
+export const Cave_Rat=new MonsterData("Cave Rat", "洞鼠", 300, 70, 105, 10, 5, 100, 350, 50, 500);
+
+export const Goblin=new MonsterData("Goblin", "哥布林", 400, 40, 95, 30, 15, 40, 150, 50, 510);
+
+export const Mimic=new MonsterData("Mimic", "模仿者", 500, 40, 85, 20, 25, 40, 150, 50, 520);
+
+export const Masked_Goblin=new MonsterData("Masked Goblin", "谜之哥布林", 400, 50, 65, 20, 15, 50, 150, 50, 540);
+
+export const Black_Aardvark=new MonsterData("Black Aardvark", "暗黑食蚁兽", 400, 70, 75, 30, 25, 50, 150, 50, 590);
+
+export const Black_Wolf=new MonsterData("Black Wolf", "黑狼", 520, 80, 85, 30, 15, 60, 150, 50, 600);
+
+export const Brown_Dire_Wolf=new MonsterData("Brown Dire Wolf", "棕土狼", 570, 50, 115, 20, 25, 50, 150, 50, 640);
+
+export const Young_Brown_Warg=new MonsterData("Young Brown Warg", "棕野狼", 470, 110, 115, 30, 15, 50, 150, 50, 660);
+
+export const Bandersnatch=new MonsterData("Bandersnatch", "巨灵", 470, 50, 135, 40, 25, 50, 150, 50, 679);
+
+export const Blue_Snake=new MonsterData("Blue Snake", "寒炎蛇", 870, 90, 115, 40, 25, 60, 150, 50, 700);
+
+export const Kobold=new MonsterData("Kobold", "科多兽", 1070, 130, 150, 40, 25, 60, 150, 50, 750);
+
+export const Rat_Man=new MonsterData("Rat Man", "鼠人", 970, 130, 155, 20, 35, 60, 150, 50, 760);
+
+export const Red_Spider=new MonsterData("Red Spider", "赤血蜘蛛", 1200, 110, 140, 30, 35, 60, 150, 50, 800);
+
+export const White_Hair_Llama=new MonsterData("White Hair Llama", "白发血马", 1000, 140, 165, 40, 15, 70, 150, 50, 850);
+
+export const Kobold_Bandit=new MonsterData("Kobold Bandit", "地精劫持者", 1000, 100, 155, 60, 5, 70, 150, 50, 860);
+
+export const Coyote=new MonsterData("Coyote", "暴动者", 800, 100, 150, 10, 10, 170, 150, 50, 870);
+
+export const Zombie_Soldier=new MonsterData("Zombie Soldier", "僵尸士兵", 900, 150, 155, 20, 20, 100, 150, 50, 900);
+
+export const White_Bear=new MonsterData("White Bear", "白熊", 700, 100, 150, 20, 15, 70, 150, 50, 930);
+
+export const Maned_Aardvark=new MonsterData("Maned Aardvark", "鬃毛食蚁兽", 750, 80, 125, 70, 55, 70, 150, 50, 940);
+
+export const Stone_Hound=new MonsterData("Stone Hound", "石化者", 790, 200, 215, 0, 0, 220, 350, 50, 954);
+
+export const Goblin_Keeper=new MonsterData("Goblin Keeper", "哥布林守护者", 870, 100, 150, 50, 35, 70, 150, 50, 960);
+
+export const Bard_Skeleton=new MonsterData("Bard Skeleton", "吟游骷髅", 970, 100, 125, 40, 35, 80, 150, 50, 989);
+
+export const Burgundy_Spider=new MonsterData("Burgundy Spider", "血色天狼蛛", 780, 100, 255, 10, 15, 150, 250, 50, 1000);
+
+export const Giant_Forest_Rat=new MonsterData("Giant Forest Rat", "巨魔鼠", 1230, 100, 150, 40, 35, 80, 150, 50, 1040);
+
+export const Gold_Goblin=new MonsterData("Gold Goblin", "黄金哥布林", 1150, 100, 175, 50, 35, 80, 150, 50, 1050);
+
+export const Gold_Kobold=new MonsterData("Gold Kobold", "黄金食蚁兽", 1200, 100, 157, 70, 55, 80, 150, 50, 1100);
+
+export const Gray_Gremlin=new MonsterData("Gray Gremlin", "阴霾小鬼", 1370, 120, 150, 50, 25, 80, 150, 50, 1140);
+
+export const Young_Lungfish=new MonsterData("Young Lungfish", "地肺鱼", 290, 310, 415, 0, 35, 220, 250, 50, 1160);
+
+export const Jackal=new MonsterData("Jackal", "豺狼", 870, 210, 215, 100, 5, 80, 150, 50, 1200);
+
+export const Stripeless_Hyena=new MonsterData("Stripeless Hyena", "斑纹土狗", 590, 210, 315, 10, 75, 120, 150, 50, 1240);
+
+export const Phantom_Silver_Tableware=new MonsterData("Phantom Silver Tableware", "幻影战士", 1000, 110, 135, 50, 55, 80, 150, 50, 1250);
+
+export const Stone_Mask=new MonsterData("Stone Mask", "土元素", 1600, 150, 215, 100, 15, 80, 150, 50, 1250);
+
+export const Dragonfly=new MonsterData("Dragonfly", "龙蝇", 1800, 150, 215, 100, 5, 80, 150, 50, 1290);
+
+export const Imp=new MonsterData("Imp", "小恶魔", 1800, 160, 215, 10, 75, 80, 150, 50, 1300);
+
+export const Ice_Sprite=new MonsterData("Ice Sprite", "冰精灵", 1900, 160, 215, 20, 85, 80, 150, 50, 1350);
+
+export const Lightning_Sprite=new MonsterData("Lightning Sprite", "雷精灵", 2000, 210, 215, 50, 45, 80, 150, 50, 1400);
+
+export const Red_Lynx=new MonsterData("Red Lynx", "红猞猁", 2000, 180, 215, 10, 55, 80, 150, 50, 1400);
+
+export const Skeleton=new MonsterData("Skeleton", "玉骷髅", 2000, 190, 215, 30, 25, 80, 150, 50, 1400);
+
+export const Candle_Warrior=new MonsterData("Candle Warrior", "浴血武者", 2070, 210, 215, 120, 5, 80, 150, 50, 1450);
+
+export const Guardian_Horse_of_Ruins=new MonsterData("Guardian Horse of Ruins", "毁灭守卫", 2570, 210, 215, 120, 5, 80, 150, 50, 1450);
+
+export const Gorgon=new MonsterData("Gorgon", "蛇发女怪", 4040, 130, 200, 50, 85, 80, 150, 50, 1550);
+
+export const Stone_Horse=new MonsterData("Stone Horse", "石象鬼", 2590, 160, 215, 80, 5, 90, 150, 50, 1550);
+
+export const Topaz_Beetle=new MonsterData("Topaz Beetle", "灯怪", 3470, 150, 200, 80, 15, 90, 150, 50, 1550);
+
+export const Brown_Bear=new MonsterData("Brown Bear", "棕熊", 1500, 100, 300, 200, 85, 20, 150, 50, 1550);
+
+export const Red_Kobold=new MonsterData("Red Kobold", "噬血兽", 2000, 100, 315, 10, 5, 200, 250, 50, 1550);
+
+export const Stone_Zombie=new MonsterData("Stone Zombie", "硬化僵尸", 4000, 100, 150, 100, 5, 100, 150, 50, 1600);
+
+export const Black_Buffalo=new MonsterData("Black Buffalo", "黑魔导", 3000, 150, 255, 10, 45, 100, 150, 50, 1700);
+
+export const Shrieker=new MonsterData("Shrieker", "花妖", 2800, 200, 215, 40, 25, 90, 150, 50, 1670);
+
+export const Long_Horn_Gnu=new MonsterData("Long Horn Gnu", "树妖", 4000, 150, 265, 40, 25, 80, 150, 50, 1650);
+
+export const Wisp=new MonsterData("Wisp", "小精灵", 6000, 290, 305, 90, 45, 90, 150, 50, 1700);
+
+export const Saturos=new MonsterData("Saturos", "石虎", 7000, 300, 405, 40, 70, 80, 150, 50, 1740);
+
+export const Skeleton_Ghost=new MonsterData("Skeleton Ghost", "骷髅幽魂", 9030, 300, 405, 130, 5, 90, 150, 50, 1800);
+
+export const Guardian_of_Ruins=new MonsterData("Guardian of Ruins", "马面", 8070, 10, 600, 150, 5, 100, 150, 50, 1850);
+
+export const Black_Succubus=new MonsterData("Black Succubus", "牛头", 5040, 100, 905, 300, 5, 190, 150, 50, 1900);
+
+export const Giant_Spider=new MonsterData("Giant Spider", "天兵", 3520, 320, 515, 300, 55, 100, 150, 50, 1900);
+
+export const Stone_Horse_Keeper=new MonsterData("Stone Horse Keeper", "蛤蟆精", 7070, 300, 515, 210, 5, 100, 150, 50, 1900);
+
+export const Troll=new MonsterData("Troll", "蛇妖", 10070, 510, 715, 20, 45, 100, 150, 50, 2000);
+
+export const Gnoll=new MonsterData("Gnoll", "蝎子精", 12030, 500, 815, 10, 55, 100, 150, 50, 2010);
+
+export const Magic_Golem=new MonsterData("Magic Golem", "蛟龙", 12000, 600, 805, 10, 65, 100, 150, 50, 2020);
+
+export const Captain_Skeleton=new MonsterData("Captain Skeleton", "风伯", 14000, 500, 905, 10, 75, 100, 150, 50, 2030);
+
+export const Green_Snake=new MonsterData("Green Snake", "芙蓉仙子 ", 7000, 1000, 1505, 10, 25, 200, 150, 50, 2100);
+
+export const Lost_Sahuagin=new MonsterData("Lost Sahuagin", "大力金刚", 30040, 600, 1000, 300, 35, 100, 150, 50, 2200);
+
+export const Hippo=new MonsterData("Hippo", "雾中仙", 40000, 300, 1200, 300, 35, 100, 150, 50, 2300);
+
+export const Brown_Ixion=new MonsterData("Brown Ixion", "噬天虎 ", 30000, 400, 1300, 200, 25, 150, 200, 50, 2300);
+
+export const Incubus=new MonsterData("Incubus", "净瓶女娲", 35000, 400, 1115, 10, 45, 90, 150, 50, 2400);
+
+export const Zombie=new MonsterData("Zombie", "鬼将", 35000, 500, 1305, 10, 55, 100, 150, 50, 2550);
+
+export const Bisque_Doll=new MonsterData("Bisque Doll", "地狱战神", 37000, 700, 1005, 10, 65, 100, 150, 50, 2700);
+
+export const Ogre=new MonsterData("Ogre", "狐狸精", 20000, 500, 1500, 10, 55, 120, 150, 50, 2800);
+
+export const Esras=new MonsterData("Esras", "巴古贡", 19990, 800, 1405, 10, 5, 320, 350, 50, 2900);
+
+export const Ogre_Warrior=new MonsterData("Ogre Warrior", "魔战士", 50000, 1000, 1215, 10, 5, 220, 250, 50, 3050);
+
+export const Giant_Ogre=new MonsterData("Giant Ogre", "巨灵", 20000, 1000, 1800, 110, 55, 100, 150, 50, 3100);
+
+export const Siren=new MonsterData("Siren", "活死人", 8000, 1400, 2100, 310, 95, 100, 150, 50, 3200);
+
+export const Lion=new MonsterData("Lion", "天狮", 50000, 1000, 1899, 10, 25, 150, 250, 50, 3300);
+
+export const Balrog=new MonsterData("Balrog", "巴洛克", 70000, 1200, 1600, 10, 35, 120, 250, 50, 3400);
+
+export const Cyclops=new MonsterData("Cyclops", "独眼巨人", 75900, 1300, 1500, 10, 45, 120, 250, 50, 3440);
+
+export const Argus=new MonsterData("Argus", "百眼巨人", 59070, 1, 3000, 10, 55, 120, 250, 50, 3500);
+
+export const Grendel=new MonsterData("Grendel", "格伦德尔", 90070, 2000, 2015, 400, 5, 120, 250, 50, 3650);
+
+export const Cloaker=new MonsterData("Cloaker", "蛰伏伪怪", 40070, 2010, 2215, 610, 5, 120, 250, 50, 3760);
+
+export const Wight=new MonsterData("Wight", "尸妖", 99999, 1500, 2005, 50, 35, 120, 250, 50, 3900);
+
+export const Ghost_Cloaker=new MonsterData("Ghost Cloaker", "魂使者", 99999, 1500, 2005, 10, 35, 120, 150, 50, 4000);
+
+export const Black_Warrior=new MonsterData("Black Warrior", "黑武士", 99999, 1500, 2225, 10, 35, 120, 150, 50, 4000);
+
+export const Pink_Succubus=new MonsterData("Pink Succubus", "樱花女妖", 99999, 1660, 2215, 10, 35, 120, 150, 50, 4300);
+
+export const Spider_Warrior=new MonsterData("Spider Warrior", "蜘蛛人", 100000, 2000, 2315, 10, 35, 120, 150, 50, 4500);
+
+export const Head_Hyena=new MonsterData("Head Hyena", "噬脑者", 100000, 2110, 2315, 10, 45, 120, 150, 50, 5000);
+
+export const Hellcat=new MonsterData("Hellcat", "地狱守护者", 150000, 2110, 2415, 10, 45, 120, 150, 50, 5500);
+
+export const Salamander=new MonsterData("Salamander", "溶岩蜥蜴", 120000, 2210, 2415, 10, 45, 120, 150, 50, 6500);
+
+export const Banshee=new MonsterData("Banshee", "魅惑女妖", 30000, 2210, 3015, 410, 125, 120, 150, 50, 6500);
+
+export const Ruairi=new MonsterData("Ruairi", "虚空守卫", 120000, 2010, 2555, 10, 35, 120, 150, 50, 7500);
+
+export const Yeti=new MonsterData("Yeti", "神灵", 50000, 2210, 3315, 10, 45, 20, 150, 50, 7500);
+
+export const Mammoth=new MonsterData("Mammoth", "宙斯", 150000, 2010, 2515, 110, 55, 120, 150, 50, 8000);
+
+export const Giant_Sand_Worm=new MonsterData("Giant_Sand_Worm", "深渊武士", 70000, 2010, 2515, 100, 55, 120, 150, 50, 9000);
+
+export const Ifrit=new MonsterData("Ifrit", "恐怖利刃", 270000, 2010, 3015, 210, 75, 120, 150, 50, 10000);
+
+export const Prairie_Dragon=new MonsterData("Prairie Dragon", "空间掌控者", 300000, 3010, 4005, 10, 125, 150, 150, 50, 22000);
+
+export const Giant_Lion=new MonsterData("Giant Lion", "时间掌控者", 500000, 4010, 5515, 510, 45, 150, 150, 50, 25000);
+
+export const Arc_Lich=new MonsterData("Arc Lich", "永生神王", 700000, 5510, 6615, 1000, 25, 250, 150, 50, 27000);
+
+export const Desert_Dragon=new MonsterData("Desert Dragon", "世纪之龙", 900000, 5010, 8015, 600, 95, 250, 250, 90, 30000);
