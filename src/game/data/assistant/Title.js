19d5735c7c8ecf5fec2d6f678f825303a7d6aaa7
IplayLF2 <914637118@qq.com>
2019-03-31 22:59:24 +0800

    playerTitle数据

--- src/game/data/assistant/Title.js
+++ src/game/data/assistant/Title.js
@@ -1,13 +1,17 @@
+var count = 0;
 export default class Title {
-    constructor(name, realName, description, statMulList, maxFix, countFix) {
+    constructor(name, realName, description, statMulList, maxFix = 0, countFix = 0) {
+        this.id = count++;
         this.name = name;
         this.realName = realName;
         this.description = description;
         this.statMulList = statMulList;
         this.maxFix = maxFix;
         this.countFix = countFix;
+
+        this.statMulList.map(s => { s.mId = this.id; });
     }
     toData() {
-        return [this.name, this.realName, this.description, this.statMulList, this.maxFix, this.countFix];
+        return [this.id, this.name, this.realName, this.description, this.maxFix, this.countFix];
     }
 }
