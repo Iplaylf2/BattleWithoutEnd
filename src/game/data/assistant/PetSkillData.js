624d1ab0401a236aca6ea564d71e7a0a4576518f
IplayLF2 <914637118@qq.com>
2019-03-31 22:46:02 +0800

    petSkill数据

--- src/game/data/assistant/PetSkillData.js
+++ src/game/data/assistant/PetSkillData.js
@@ -1,10 +1,12 @@
+var count = 0;
 export default class PetSkillData {
     constructor(name, realName, setList) {
+        this.id = count++;
         this.name = name;
         this.realName = realName;
         this.setList = setList;
     }
     toData() {
-        return [this.name, this.realName, this.setList];
+        return [this.id, this.name, this.realName, this.setList];
     }
 }
