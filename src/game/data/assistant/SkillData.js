eb979658ae29ce1f965fd1e5174712c3ca220348
IplayLF2 <914637118@qq.com>
2019-03-31 22:40:55 +0800

    peopleSkill

--- src/game/data/assistant/SkillData.js
+++ src/game/data/assistant/SkillData.js
@@ -1,13 +1,15 @@
+var count = 0;
 export default class SkillData {
     constructor(name, realName, category, statList, effectList, lvupCostList) {
+        this.id = count++;
         this.name = name;
         this.realName = realName;
         this.category = category;
         this.statList = statList;
-        this.effectList = effectList;
+        this.effectList = effectList || [];
         this.lvupCostList = lvupCostList;
     }
     toData() {
-        return [this.name, this.realName, this.category, this.statList, this.effectList, this.lvupCostList];
+        return [this.id, this.name, this.realName, this.category, this.statList.map(l => l.map(s => s.toData())), this.effectList.map(l => l.map(s => s.toData())), this.lvupCostList];
     }
 }
